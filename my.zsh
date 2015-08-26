f52abb9e53e70430c5fa88bd51c1c45f86292d1a
Cory Knapp <knapp.cory@gmail.com>
2015-08-25 20:53:37 -0700

    some zsh stuff I use.

---
+++ my.zsh
@@ -0,0 +1,16 @@
+function pfd() {
+  osascript 2>/dev/null <<EOF
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell
+EOF
+}
+
+function cdf() {
+  cd "$(pfd)"
+}
+
+
+
+alias vt = 'mvim -c "vsplit test.cpp"'
+alias v = 'mvim'

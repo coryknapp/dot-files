dbb27e6b62bae67a7f637fe4b806c363cd277113
Cory Knapp <knapp.cory@gmail.com>
2015-09-02 12:09:13 -0700

    Added a really cool command to change working directory to the container of the top document in vim.

--- my.zsh
+++ my.zsh
@@ -1,3 +1,5 @@
+export PATH="/usr/texbin":$PATH
+
 function pfd() {
   osascript 2>/dev/null <<EOF
     tell application "Finder"
@@ -6,11 +8,27 @@ function pfd() {
 EOF
 }
 
+function topMacVimTitle() {
+  osascript 2>/dev/null <<EOF
+tell application "MacVim"
+	return name of window 1
+end tell
+EOF
+}
+
 function cdf() {
   cd "$(pfd)"
 }
 
-
+function cdv() {
+local dir="$(echo $(topMacVimTitle) | grep -o "(.*)" | sed 's/(//' | sed 's/)//')"
+dir="${dir/#\~/$HOME}" #expand the ~
+cd $dir
+}
 
 alias vt='mvim -c "vsplit test.cpp"'
 alias v='mvim'
+
+alias j='java'
+alias jc = 'javac'
+alias jd='javadoc -d docs'

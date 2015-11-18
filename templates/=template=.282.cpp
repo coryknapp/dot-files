dcf3e8bd62edabb413fe53d96d4149f9dd5d89a9
Cory Knapp <knapp.cory@gmail.com>
2015-11-17 16:01:16 -0800

    Added template for my 282 cpp class.

---
+++ templates/=template=.282.cpp
@@ -0,0 +1,17 @@
+//Cory Knapp
+//CECS 282 Sec 03 5082 C++ for Java Programmers
+//%DATE%
+
+#ifndef %GUARD%
+#define %GUARD%
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	%HERE%
+
+	return 0;
+}
+
+#endif // #ifndef %GUARD%

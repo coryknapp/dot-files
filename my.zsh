988bb878037ba0438ab57f82f1af3e2774de7da8
Cory Knapp <knapp.cory@gmail.com>
2016-09-07 12:59:55 -0700

    added paths for mono (c# stuff)

--- my.zsh
+++ my.zsh
@@ -1,4 +1,6 @@
 export PATH="/usr/texbin":$PATH
+export PATH=/Library/Frameworks/Mono.framework/Versions/Current/bin/:${PATH}
+
 function pfd() {
   osascript 2>/dev/null <<EOF
     tell application "Finder"
